50239a867aedb2525168f3232c13e2fa34bbe1dd
NoxusJr <ctt.georgejr@gmail.com>
2024-05-19 17:07:51 -0300

    Add Request logic in Translate.java

--- code/src/main/java/com/georgejrdev/core/Translate.java
+++ code/src/main/java/com/georgejrdev/core/Translate.java
@@ -1,11 +1,5 @@
 package com.georgejrdev.core;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import com.georgejrdev.core.interfaces.TranslateInterface;
 
 
@@ -15,30 +9,9 @@ public class Translate implements TranslateInterface {
 
     @Override
     public String translate(String text, String lang){
+        Request request = new Request();
         final String REQUEST_URL = URL_API + text + SEPARATOR + lang;
 
-        try{
-            String inputLine;
-
-            URL url = new URL(REQUEST_URL);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            StringBuilder response = new StringBuilder();
-            
-            while ((inputLine = in.readLine()) != null){
-                response.append(inputLine);
-            }
-
-            in.close();
-            connection.disconnect();
-
-            return response.toString();
-        } catch (FileNotFoundException e) {
-            return e.toString();
-        } catch (Exception e){
-            return e.toString();
-        }
+        return request.getResultRequest(REQUEST_URL,"GET");
     }
 }
