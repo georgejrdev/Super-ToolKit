5aa990a7d4e4b877f2d632d2ae0f7d137c472ff2
NoxusJr <ctt.georgejr@gmail.com>
2024-05-19 01:16:23 -0300

    Implemented Help interface

---
+++ code/src/main/java/com/georgejrdev/core/Help.java
@@ -0,0 +1,29 @@
+package com.georgejrdev.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.georgejrdev.core.interfaces.HelpInterface;
+
+
+public class Help implements HelpInterface{
+    
+    final Map<String,String> COMMAND_SYNTAX = new HashMap<String,String>() {{
+        put("-h","tkd -h");
+        put("-t","tkd -t 'Text' pt");
+        put("-c","tkd -c 'Text'");
+    }};
+
+
+    @Override
+    public void showCommands(){
+        System.out.println("tkd -h -> Show all commands");
+        System.out.println("tkd -t -> Translate a text");
+        System.out.println("tkd -c -> Chat with ChatGPT");
+    }
+
+    @Override
+    public void showCorrectSintax(String command){
+        System.out.println("Syntax Error: the correct syntax is -> " + COMMAND_SYNTAX.get(command));
+    }
+}
--- code/src/main/java/com/georgejrdev/core/Translate.java
+++ code/src/main/java/com/georgejrdev/core/Translate.java
@@ -1,4 +1,5 @@
 package com.georgejrdev.core;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
