60a151563cc368510ad93f499aef6c65208b1ce3
NoxusJr <ctt.georgejr@gmail.com>
2024-08-27 18:58:13 -0300

    Create Exceptions

---
+++ src/main/java/com/georgejrdev/utils/exceptions/InvalidOptionCommand.java
@@ -0,0 +1,24 @@
+package com.georgejrdev.utils.exceptions;
+
+
+public class InvalidOptionCommand extends Exception {
+    
+    public InvalidOptionCommand(){
+        super();
+    }
+
+    
+    public InvalidOptionCommand(String message){
+        super(message);
+    }
+
+
+    public InvalidOptionCommand(String message, Throwable cause){
+        super(message, cause);
+    }
+
+
+    public InvalidOptionCommand(Throwable cause){
+        super(cause);
+    }
+}
