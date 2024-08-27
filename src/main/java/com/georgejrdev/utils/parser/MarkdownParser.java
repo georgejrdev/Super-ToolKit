f4b9d2bb360096a5db043289adfd2c01657ccac0
NoxusJr <ctt.georgejr@gmail.com>
2024-08-27 18:59:54 -0300

    Create MarkdownParser

---
+++ src/main/java/com/georgejrdev/utils/parser/MarkdownParser.java
@@ -0,0 +1,11 @@
+package com.georgejrdev.utils.parser;
+
+import com.georgejrdev.auxiliar.reload.Parser;
+
+
+public class MarkdownParser implements Parser{
+    
+    public String parse(String text){
+        return text;
+    }
+}
